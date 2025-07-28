4c08e5a16ed1bb5531cb6b83ef0d854c9958769a
Aditya Babar <adityabuzz331@gmail.com>
2025-07-28 12:34:31 +0530

    work in progress:setup accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from .views import RegisterView, LoginView,LogoutView, UserProfileView
 
-
-
-
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
