55bbb5b47255359cb2b73a1a154d99d6d01b2413
Aditya Babar <adityabuzz331@gmail.com>
2025-07-31 13:18:40 +0530

    finish banner w templates

--- banner/models.py
+++ banner/models.py
@@ -12,7 +12,7 @@ class Template(models.Model):
 
     title = models.CharField(max_length=255)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
-    template_image = models.CharField(max_length=255)  # or use ImageField if handling file uploads
+    template_image = models.ImageField(upload_to='templates/')  # or use ImageField if handling file uploads
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -23,12 +23,12 @@ class Template(models.Model):
 
 class Banner(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    template = models.ForeignKey(Template, on_delete=models.SET_NULL, null=True, blank=True)
-    banner_image = models.CharField(max_length=255)  # or use ImageField
+    template = models.ForeignKey(Template, on_delete=models.SET_NULL, null=True, blank=True, related_name='banners')
+    banner_image = models.ImageField(upload_to='banners/')  # or use ImageField
     text_content = models.TextField(blank=True, null=True)
     custom_name = models.CharField(max_length=100, blank=True, null=True)
-    custom_logo = models.CharField(max_length=255, blank=True, null=True)  # or ImageField
-    custom_photo = models.CharField(max_length=255, blank=True, null=True)  # or ImageField
+    custom_logo = models.ImageField(upload_to='logos/',blank=True,null=True)  # or ImageField
+    custom_photo = models.ImageField(upload_to='photos/',blank=True, null=True)  # or ImageField
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
