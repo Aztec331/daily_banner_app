b13495123461185f5e5687a51740a73c283f35a3
Aditya Babar <adityabuzz331@gmail.com>
2025-08-13 12:42:33 +0530

    babar merge everything

--- banner/views.py
+++ banner/views.py
@@ -72,7 +72,7 @@ class BannerDeleteView(generics.DestroyAPIView):
         return Banner.objects.filter(user=self.request.user)
 
 class BannerDownloadView(APIView):
-    permission_classes= [IsAuthenticated]
+    permission_classes= [AllowAny]
     def get(self, request, pk):
         try:
             banner = Banner.objects.get(pk=pk)
