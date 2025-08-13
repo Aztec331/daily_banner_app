8cdc1b78389fdd822f7731ae825c6a3223f69baa
shreyas <shreyasvkau@gmail.com>
2025-08-13 13:56:38 +0530

    made new changes

--- daily_banner_app/settings.py
+++ daily_banner_app/settings.py
@@ -115,16 +115,20 @@ MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'daily_banner_db',
-#         'USER':'banner_user',
-#         'PASSWORD': 'DailyBannerApp01',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'daily_banner_db',
+        'USER': 'banner_user',
+        'PASSWORD': 'DailyBannerApp01',
+        'HOST': 'postgresql://daily_banner_app_user:ISNQtyeICuB8oa0gw9bNQBqCSvwQIHst@dpg-d2e4kaggjchc73e13om0-a.oregon-postgres.render.com/daily_banner_app',  # use your actual host here
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require'
+        }
+    }
+}
+
 
 # DATABASES = {
 #     'default': dj_database_url.config(
